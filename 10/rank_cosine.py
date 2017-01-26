9e9eabb344d0284867f89fdf3e47f7f8b2c7dfb9
alex <Alexander.Markin@lmu.de>
2017-01-26 15:12:15 +0100

    rank

---
+++ 10/rank_cosine.py
@@ -0,0 +1,40 @@
+import re, math
+from collections import Counter
+
+WORD = re.compile(r'\w+')
+cosines = dict()
+
+def get_cosine(vec1, vec2):
+     intersection = set(vec1.keys()) & set(vec2.keys())
+     numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+     sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+     sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+     denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+     if not denominator:
+        return 0.0
+     else:
+        return float(numerator) / denominator
+
+def text_to_vector(text):
+     words = WORD.findall(text)
+     return Counter(words)
+
+text = input("Enter query, or just hit ENTER to quit.")
+
+
+vector = text_to_vector(text)
+with open("donald100.txt", encoding="utf") as readfile:
+    for line in readfile:
+       cosine = get_cosine(text_to_vector(line), vector)
+       if(cosine > 0):
+        cosines[cosine] = line
+
+reversedDict = sorted(cosines.keys())
+topTenList = sorted(cosines, key = cosines.get)[-10:]
+
+for i in reversed(sorted(cosines.keys())):
+    print(i, ':' , cosines[i])
+
+
