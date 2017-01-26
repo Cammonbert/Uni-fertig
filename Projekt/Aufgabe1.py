562004f4640bb7700d368802e2e2b763934bfbf2
Adrian <reitza@cip.ifi.lmu.de>
2017-01-26 15:51:12 +0100

    Upload new file

---
+++ Projekt/Aufgabe1.py
@@ -0,0 +1,9 @@
+with open("Linktext.txt", "a", encoding="utf-8") as writefile:
+    with open("wikilinks_en.txt", encoding="utf-8") as readfile:
+        for line in readfile:
+            if "|" in line:
+                line = line.replace("[", "", 2)
+                line = line.replace("|", "        ", 1)
+                line = line.replace("]", "", 2)
+                writefile.write(line)
+    writefile.write("\n")
