3b71d1a6828fa3117627977e05a4cead056124d4
Iryna Matviienko <vieniry@gmail.com>
2017-01-31 00:15:52 +0100

    Projekt: A3: Korrektur

--- Projekt/Aufgabe3_int.py
+++ Projekt/Aufgabe3_int.py
@@ -1,42 +1,45 @@
-import re
+import sys
 
+def outputfile_open():
+    try:
+        if len(sys.argv) > 3:
+            fh = open(sys.argv[3], "w")
+        else:
+            fh = sys.stdout
+        return fh
+    except IOError:
+        print("Error: could not open output file for writing!")
 
-def stringsucheinter():
-    reg = r"(\d+)([ , ]{3})(.+)([ ]{8})(.+)"
-    list2 = list()
-    eingabe = input("String: ")
+#Starts here
+searchwords = dict()
+dictText = dict()
+
+if len(sys.argv) > 1:
+    with open(sys.argv[1], encoding="utf-8") as swfile:
+        for i in swfile:
+            searchwords[i.rstrip()] = 0
+
+if len(searchwords) == 0:
+    eingabe = ""
     while eingabe != " ":
-        with open("Over200.txt", encoding="utf-8") as readfile:
-            for line in readfile:
-                list1 = re.findall(reg, line)
-                for item in list1:
-                    if eingabe == item[4]:
-                        list2.append((item[2], int(item[0])))
-        if list2:
-            print(sorted(list2, key=lambda x: x[1], reverse=True)[0][0])
-        list2.clear()
-        eingabe = input("string: ")
+        eingabe = input("Enter search string: ")
+        if eingabe != " ":
+            searchwords[eingabe] = 0
 
+linkstatistikfilename = "Linkstatistik.txt"
+if len(sys.argv) > 2:
+    linkstatistikfilename = sys.argv[2]
 
-def stringsuchetext(datei):
-    reg = r"(\d+)([ , ]{3})(.+)([ ]{8})(.+)"
-    list2 = list()
-    writefile = open("Stringsucheergebnis.txt", "w", encoding="Utf-8")
-    writefile.truncate()
-    writefile.close()
-    writefile = open("Stringsucheergebnis.txt", "a", encoding="Utf-8")
-    eingabedatei = open(datei, "r", encoding="Utf-8")
-    for x in eingabedatei.readlines():
-        eingabe = x
-        with open("Over200.txt", encoding="utf-8") as readfile:
-            for line in readfile:
-                list1 = re.findall(reg, line)
-                for item in list1:
-                    if eingabe == item[4] + "\n":
-                        list2.append((item[2], int(item[0])))
-        if list2:
-            writefile.write(sorted(list2, key=lambda x: x[1], reverse=True)[0][0] + "\n")
-        list2.clear()
+with open(linkstatistikfilename, encoding="utf-8") as readfile:
+    for i in readfile:
+        tuple = i.rstrip().split("\t")
+        if len(tuple) > 2:
+            if tuple[2] in searchwords:
+                if int(tuple[0]) > searchwords[tuple[2]]:
+                    searchwords[tuple[2]] = int(tuple[0])
+                    dictText[tuple[2]] = tuple[1]
 
-
-stringsuchetext("names_line_by_line.txt")
+with outputfile_open() as outfile:
+    for i in searchwords:
+        if i in dictText:
+            outfile.write(str(dictText[i]) + "\n")
