10218e351e7667476d246e48b9205cc4b81f49d2
Iryna Matviienko <vieniry@gmail.com>
2017-01-30 15:14:51 +0100

    Projekt: A2b: Korrektur

--- Projekt/Aufgabe2.2.py
+++ Projekt/Aufgabe2.2.py
@@ -1,11 +1,13 @@
-dictionary = dict()
-with open("Linktext.txt", encoding="utf-8") as readfile:
+dictRepetition = dict()
+dictText = dict()
+with open("Linkstatistik.txt", encoding="utf-8") as readfile:
     for i in readfile:
-        if i in dictionary:
-            dictionary[i] += 1
-        else:
-            dictionary[i] = 1
-with open("Over200.txt", "a", encoding="utf-8") as writefile:
-    for i in sorted(dictionary):
-        if dictionary[i] >= 200:
-            writefile.write(str(dictionary[i]) + " , " + i)
+        tuple = i.rstrip().split("\t")
+        if len(tuple) > 2:
+            dictRepetition[tuple[1]] = dictRepetition.get(tuple[1], 0) + int(tuple[0])
+            dictText[tuple[1]] = tuple[2]
+
+with open("Over200.txt", "w", encoding="utf-8") as writefile:
+    for i in sorted(dictRepetition):
+        if dictRepetition[i] >= 200:
+	        writefile.write(str(dictRepetition[i]) + "\t" + i + "\t" + dictText[i] + "\n")
