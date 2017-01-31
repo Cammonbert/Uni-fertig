e4af7ad16da2c3f64201f14ae8711476545ecdab
Iryna Matviienko <vieniry@gmail.com>
2017-01-31 16:57:01 +0100

    Projekt: A4: Korrektur

--- Projekt/Aufgabe4.py
+++ Projekt/Aufgabe4.py
@@ -1,73 +1,108 @@
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
-import re
+import sys
+import string
 
+def outputfile_open():
+    try:
+        if len(sys.argv) > 3:
+            fh = open(sys.argv[3], "w")
+        else:
+            fh = sys.stdout
+        return fh
+    except IOError:
+        print("Error: could not open output file for writing!")
 
-jar = 'C:/Users/Adri/stanford-ner-2016-10-31/stanford-ner.jar'
-model = 'C:/Users/Adri/stanford-ner-2016-10-31/classifiers/english.all.3class.distsim.crf.ser.gz'
-
-
-def interactive():
-    reg = r"(\d+)([ , ]{3})(.+)([ ]{8})(.+)"
-    list2 = list()
+def analyseSentenses(inputList, linkstatistikfilename):
+    jar = 'stanford-ner-2016-10-31/stanford-ner.jar'
+    model = 'stanford-ner-2016-10-31/classifiers/english.all.3class.distsim.crf.ser.gz'
     st = StanfordNERTagger(model, jar)
-    sentence = input("Enter sentence, or Leer for quit.")
-    satz = ""
-    while sentence != " ":
+
+    # create list with words to look up
+    searchwords = dict()
+    for sentence in inputList:
         sentence = word_tokenize(sentence)
-        print(st.tag(sentence))
         tags = st.tag(sentence)
+
+        prevtup = ('', '')
         for tup in tags:
-            if tup[1] == "O":
-                satz += str((tup[0] + " "))
+            if tup[1] == prevtup[1]:
+                if tup[1] != "O":
+                    prevtup = (str(prevtup[0] + " " + tup[0]), tup[1])
             else:
-                with open("Over200.txt", encoding="utf-8") as readfile:
-                    for line in readfile:
-                        list1 = re.findall(reg, line)
-                        for item in list1:
-                            if tup[0] == item[4]:
-                                list2.append((item[2], int(item[0])))
-                if list2:
-                    satz += str("[[" + (sorted(list2, key=lambda x: x[1], reverse=True)[0][0] + "|" + tup[0] + "]] "))
-                    list2.clear()
-                else:
-                    satz += str((tup[0] + " "))
-        print(satz)
+                if prevtup[0] != "" and prevtup[1] != "O":
+                    searchwords[prevtup[0]] = 0
+                prevtup = (tup[0], tup[1])
+        if prevtup[0] != "" and prevtup[1] != "O":
+            searchwords[prevtup[0]] = 0
+
+    #create ranking
+    dictText = dict()
+    with open(linkstatistikfilename, encoding="utf-8") as readfile:
+        for i in readfile:
+            tuple = i.rstrip().split("\t")
+            if len(tuple) > 2:
+                if tuple[2] in searchwords:
+                    if int(tuple[0]) > searchwords[tuple[2]]:
+                        searchwords[tuple[2]] = int(tuple[0])
+                        dictText[tuple[2]] = tuple[1]
+
+    #create sentense to return
+    outputList = list()
+
+    for sentence in inputList:
+        sentence = word_tokenize(sentence)
+        tags = st.tag(sentence)
+
         satz = ""
-        sentence = input("Enter sentence, or Leer for quit.")
+        prevtup = ('', '')
+        for tup in tags:
+            if tup[1] == prevtup[1]:
+                text = prevtup[0]
+                if tup[0] not in string.punctuation and prevtup[0] != "":
+                    text += " "
+                text += tup[0]
+                prevtup = (text, tup[1])
+            else:
+                if prevtup[1] == "O" or prevtup[0] not in dictText:
+                    if prevtup[0] != "":
+                        satz += str(prevtup[0])
+                else:
+                    if dictText[prevtup[0]] == prevtup[0]:
+                        satz += str("[[" + prevtup[0] + "]]")
+                    else:
+                        satz += str("[[" + dictText[prevtup[0]] + "|" + prevtup[0] + "]]")
+                if tup[0] not in string.punctuation and prevtup[0] != "":
+                    satz += str(" ")
+                prevtup = (tup[0], tup[1])
+
+        if prevtup[0] != "":
+            if tup[0] in dictText:
+                satz += str("[[" + dictText[prevtup[0]] + "|" + prevtup[0] + "]]")
+            else:
+                satz += str(prevtup[0])
+
+        outputList.append(satz)
+
+    return outputList
 
 
-def noninteractive(datei):
-    reg = r"(\d+)([ , ]{3})(.+)([ ]{8})(.+)"        #regex erstellen
-    list2 = list()                                  #liste erstellen
-    st = StanfordNERTagger(model, jar)              #stanfordtagger bestimmen
-    #Aufgabe4_noninteractive.txt leeren
-    writefile = open("Aufgabe4_noninteractive.txt", "w")
-    writefile.truncate()
-    writefile.close()
+#Starts here
+linkstatistikfilename = "Linkstatistik.txt"
+if len(sys.argv) > 1:
+    linkstatistikfilename = sys.argv[1]
 
-    writefile = open("Aufgabe4_noninteractive.txt", "w")    #öffne writefile
-    eingabedatei = open(datei, "r")                         #öffne eingabedatei
-    for thing in eingabedatei.readlines():                  #für jede line in eingabedatei
-        satz = word_tokenize(thing)                         #line tokenizen
-        tags = st.tag(satz)                                 #tags liste erstellen
-        writefile.write("\n\n" + str(thing) + "\n")          #tags liste in datei schreiben
-        for tup in tags:                                    #für jedes tupel in der liste tags
-            if tup[1] == "O":                               #wenn value O ist
-                writefile.write(str((tup[0] + " ")))        #schreibe key
-            else:                                           #ansonsten
-                with open("Over200.txt", encoding="utf-8") as readfile:         #öffne Over200.txt
-                    for line in readfile:                                       #für jede line darin
-                        list1 = re.findall(reg, line)                           #regexe finden
-                        for item in list1:                                      #für jedes item in liste aus regexen
-                            if tup[0] == item[4]:                               #wenn key = eingabetext
-                                list2.append((item[2], int(item[0])))           #liste 2 erstellen mit anzahl und wikiartikel
-                if list2:                                                       #wenn etwas in liste2 steht
-                    #ersten(und damit höchsten) key mit artikelname in writefile schreiben
-                    writefile.write(str("[[" + (sorted(list2, key=lambda x: x[1], reverse=True)[0][0] + "|" + tup[0] + "]] ")))
-                    list2.clear()                                               #liste2 leeren
-                else:                                                           #sonst
-                    writefile.write(str((tup[0] + " ")))                        #key einfach schreiben
+if len(sys.argv) > 2:
+    with open(sys.argv[2], encoding="utf-8") as swfile:
+        searchlines = swfile.readlines()
+        outputList = analyseSentenses(searchlines, linkstatistikfilename)
+        with outputfile_open() as outfile:
+            for i in outputList:
+                outfile.write(i + "\n")
+else:
+    eingabe = ""
+    while eingabe != " ":
+        eingabe = input("Enter sentence, or 'Space' for quit: ")
+        if eingabe != " ":
+            print(analyseSentenses([eingabe], linkstatistikfilename)[0])
 
-
-noninteractive("names_line_by_line.txt")
