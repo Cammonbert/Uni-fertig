87924878feccde3f7c1f4137da72effe57d3adeb
Iryna Matviienko <vieniry@gmail.com>
2017-02-03 16:19:04 +0100

    Projekt: A5b: Fehler in Ranking korrigiert, angepasst für die Benutzung als import lib

--- Projekt/aufgabe_5b.py
+++ Projekt/aufgabe_5b.py
@@ -3,6 +3,8 @@ import csv
 import math
 from collections import Counter
 
+import aufgabe_5a
+
 def outputfile_open():
     try:
         if len(sys.argv) > 4:
@@ -25,71 +27,87 @@ def sim(q, d):
     retVal /= vecMod
     return retVal
 
+def getDocWithMaxRating(dictTf, dictIdf, searchText):
+    dictDocRanking = getRatingList(dictTf, dictIdf, searchText)
+    return max(dictDocRanking, key=dictDocRanking.get)
+
+def getRatingList(dictTf, dictIdf, searchText):
+    dictQuery = {}
+    dictDocVector = {}
+    termFreq = {}
+    dictDocRanking = {}
+    searchwords = aufgabe_5a.stringNormalizer(searchText)
+
+    for document in dictTf:
+        dictDocVector[document] = dict()
+        for term in dictTf[document]:
+            dictDocVector[document][term] = (float(dictTf[document][term]) * float(dictIdf[term]))
+            termFreq[term] = termFreq.get(term, 0) + 1
+
+    '''
+     Create vector for query
+      Number of all documents for query = Number of all documents + 1
+      number of documents that contain term for query = number of documents that contain term + 1
+      number of times term occurs in doc for query = number of times term occurs in doc + 1
+      maxOccurrences = maxOccurrences
+    '''
+    swrFreq = Counter(searchwords)
+    maxVal = max(swrFreq.values())
+
+    for term in searchwords:
+        if term in termFreq:
+            dictQuery[term] = math.log((len(dictTf) + 1) / (termFreq[term] + 1)) * float(swrFreq[term] / maxVal)
+
+    for document in dictDocVector:
+        dictDocRanking[document] = sim(dictDocVector[document], dictQuery)
+
+    return dictDocRanking
+
+def getIdf(idffilename):
+    dictIdf = {}
+    with open(idffilename, 'r', encoding="utf-8") as csvfile:
+        idfreader = csv.reader(csvfile)
+        for row in idfreader:
+            dictIdf[row[0]] = row[1]
+
+    return dictIdf
+
+def getTf(tffilename):
+    dictTf = {}
+    with open(tffilename, 'r', encoding="utf-8") as csvfile:
+        tfreader = csv.reader(csvfile)
+        for row in tfreader:
+            docName = row.pop(0)
+            dictTf[docName] = dict()
+            while len(row) > 0:
+                freq = row.pop()
+                word = row.pop()
+                dictTf[docName][word] = freq
+    return dictTf
+
+
 #Starts here
-searchwords = []
-dictIdf = {}
-dictTf = {}
-termFreq = {}
-dictDocVector = {}
-dictQuery = {}
-dictDocRanking = {}
+if __name__ == "__main__":
+    searchwords = []
 
-tffilename = "data/tf.csv"
-if len(sys.argv) > 1:
-    tffilename = sys.argv[1]
+    tffilename = "data/tf.csv"
+    if len(sys.argv) > 1:
+        tffilename = sys.argv[1]
 
-idffilename = "data/idf.csv"
-if len(sys.argv) > 2:
-    idffilename = sys.argv[2]
+    idffilename = "data/idf.csv"
+    if len(sys.argv) > 2:
+        idffilename = sys.argv[2]
 
-if len(sys.argv) > 3:
-    with open(sys.argv[3], encoding="utf-8") as swfile:
-        searchwords = swfile.read().split(" ")
+    if len(sys.argv) > 3:
+        with open(sys.argv[3], encoding="utf-8") as swfile:
+            searchwords = swfile.read()
 
-if len(searchwords) == 0:
-    eingabe = ""
-    while eingabe != " ":
-        eingabe = input("Enter search string: ")
-        if eingabe != " ":
-            searchwords += eingabe.split(" ")
+    if len(searchwords) == 0:
+        searchwords = input("Enter search string: ")
 
-with open(idffilename, 'r', encoding="utf-8") as csvfile:
-    idfreader = csv.reader(csvfile)
-    for row in idfreader:
-        dictIdf[row[0]] = row[1]
+    if len(searchwords) == 0:
+        dictIdf = getIdf(idffilename)
+        dictTf = getTf(tffilename)
 
-with open(tffilename, 'r', encoding="utf-8") as csvfile:
-    tfreader = csv.reader(csvfile)
-    for row in tfreader:
-        docName = row.pop(0)
-        dictTf[docName] = dict()
-        while len(row) > 0:
-            freq = row.pop()
-            word = row.pop()
-            dictTf[docName][word] = freq
-
-for document in dictTf:
-    dictDocVector[document] = dict()
-    for term in dictTf[document]:
-        dictDocVector[document][term] = (float(dictTf[document][term]) * float(dictIdf[term]))
-        termFreq[term] = termFreq.get(term, 0) + 1
-
-'''
- Create vector for query
-  Number of all documents for query = Number of all documents + 1
-  number of documents that contain term for query = number of documents that contain term + 1
-  number of times term occurs in doc for query = number of times term occurs in doc + 1
-  maxOccurrences = maxOccurrences
-'''
-swrFreq = Counter(searchwords)
-maxVal = max(swrFreq.values())
-
-for term in searchwords:
-    if term in termFreq:
-        dictQuery[term] = math.log((len(dictTf) + 1)/ (termFreq[term] + 1)) * float(swrFreq[term] / maxVal)
-
-for document in dictDocVector:
-    dictDocRanking[document] = sim(dictDocVector[document], dictQuery)
-
-with outputfile_open() as outfile:
-    outfile.write(max(dictDocRanking, key=dictDocRanking.get))
+        with outputfile_open() as outfile:
+            outfile.write(getDocWithMaxRating(dictTf, dictIdf, searchwords))
